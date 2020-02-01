392eb450b451317126e552141dd85c5965c1f13a
Kate <ivaka3na@gmail.com>
2020-02-01 12:09:16 +0300

    вторая часть задания. пункты 1-2

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ document.querySelector('.map').classList.remove('map--faded');
 
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-var getPinItem = function () {
+var getPinItem = function (i) {
   var pinItem = pinTemplate.cloneNode(true);
   pinItem.querySelector('img[src]').src = adList[i].author.avatar;
   pinItem.querySelector('img[alt]').alt = 'заголовок объявления';
@@ -82,7 +82,47 @@ var getPinItem = function () {
 var pinFragment = document.createDocumentFragment();
 
 for (var i = 0; i < 8; i++) {
-  pinFragment.appendChild(getPinItem());
+  pinFragment.appendChild(getPinItem(i));
 }
 
 document.querySelector('.map__pins').appendChild(pinFragment);
+
+var cardFragment = document.createDocumentFragment();
+
+var templateCard = document.querySelector('#card').content.querySelector('.map__card');
+
+
+var convertOfferType = function (ij) {
+  switch (adList[ij].offer.type) {
+    case 'house':
+      return 'Дом';
+    case 'flat':
+      return 'Квартира';
+    case 'bungalo':
+      return 'Бунгало';
+    case 'palace':
+      return 'Дворец';
+    default:
+      return 'нет такого типа';
+  }
+};
+
+var getCardItem = function () {
+  var cardItem = templateCard.cloneNode(true);
+  cardItem.querySelector('.popup__title').textContent = adList[1].offer.title;
+  cardItem.querySelector('.popup__text--address').textContent = adList[1].offer.address;
+  cardItem.querySelector('.popup__text--price').textContent = adList[1].offer.price + '₽/ночь';
+  cardItem.querySelector('.popup__type').textContent = convertOfferType(1);
+  cardItem.querySelector('.popup__text--capacity').textContent = adList[1].offer.rooms + ' комнаты для ' + adList[1].offer.rooms + ' гостей';
+  cardItem.querySelector('.popup__text--time').textContent = 'Заезд после ' + adList[1].offer.checkin + ' выезд до ' + adList[1].offer.checkout;
+  cardItem.querySelector('.popup__features').textContent = adList[1].offer.features;
+  cardItem.querySelector('.popup__description').textContent = adList[1].offer.description;
+  cardItem.querySelector('.popup__photos img').src = 'http://o0.github.io/assets/images/tokyo/hotel1.jpg';
+  cardItem.querySelector('.popup__avatar').src = adList[1].author.avatar;
+  return cardItem;
+};
+
+cardFragment.appendChild(getCardItem());
+
+
+document.querySelector('.map__pins').appendChild(cardFragment);
