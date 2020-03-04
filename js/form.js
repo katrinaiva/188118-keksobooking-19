4e9473dc2d462e49aa117b00a93202a16e5028f1
Kate <ivaka3na@gmail.com>
2020-03-04 16:51:32 +0300

    добавила перетаскивание

--- js/form.js
+++ js/form.js
@@ -6,56 +6,10 @@
   var roomNumber = adForm.querySelector('#room_number');
   var BORDER_ERROR = '2px dashed red';
   var BORDER_DEFAULT = '1px solid #d9d9d3';
-  var mapPinMain = document.querySelector('.map__pin--main');
-  var fieldsSets = document.querySelectorAll('fieldset');
-  var selectors = document.querySelectorAll('select');
-  var mapFilters = document.querySelector('.map__filters');
-  var formFieldset = adForm.querySelectorAll('fieldset');
-  var formSelect = adForm.querySelectorAll('select');
-  var filterSelect = mapFilters.querySelectorAll('select');
-  var filterFieldset = mapFilters.querySelectorAll('fieldset');
   var timeIn = document.querySelector('#timein');
   var timeOut = document.querySelector('#timeout');
   var typeOfHousing = document.querySelector('#type');
   var priceOfHousing = document.querySelector('#price');
-
-  var toggleStates = function (formElement, isDisabled) {
-    for (var i = 0; i < formElement.length; i++) {
-      formElement[i].disabled = isDisabled;
-    }
-  };
-
-  toggleStates(fieldsSets, true);
-  toggleStates(selectors, true);
-
-  var changeDomElementState = function () {
-    document.querySelector('.map').classList.remove('map--faded');
-    adForm.classList.remove('ad-form--disabled');
-    toggleStates(formFieldset, false);
-    toggleStates(formSelect, false);
-    toggleStates(filterSelect, false);
-    toggleStates(filterFieldset, false);
-  };
-
-  var fieldAddress = document.querySelector('#address');
-  var mapPinY = parseInt(mapPinMain.style.top, 10);
-  var mapPinX = parseInt(mapPinMain.style.left, 10);
-  var mapPinMainWidth = mapPinMain.offsetWidth;
-  var mapPinMainHeight = mapPinMain.offsetHeight;
-
-  var getCoordinatesPinActive = function () {
-    var xCenterActive = mapPinX + mapPinMainWidth / 2;
-    var yCenterActive = mapPinY + mapPinMainHeight;
-    fieldAddress.value = xCenterActive + ', ' + yCenterActive;
-  };
-
-  var getCoordinatesPinDisabled = function () {
-    var xCenterDisabled = mapPinX + mapPinMainHeight / 2;
-    var yCenterDisabled = mapPinY + mapPinMainHeight / 2;
-    fieldAddress.value = xCenterDisabled + ', ' + yCenterDisabled;
-  };
-
-  getCoordinatesPinDisabled();
 
   var checkValue = function () {
     if (+capacity.value > +roomNumber.value) {
@@ -115,9 +69,4 @@
         getPriceValue('нет подходящих значений');
     }
   });
-
-  window.form = {
-    changeDomElementState: changeDomElementState,
-    getCoordinatesPinActive: getCoordinatesPinActive
-  };
 })();
--- js/map.js
+++ js/map.js
@@ -3,7 +3,15 @@
 (function () {
   var mapCard = document.querySelector('.map__card');
   var popupClose = document.querySelector('.popup__close');
+  var adForm = document.querySelector('.ad-form');
   var mapPinMain = document.querySelector('.map__pin--main');
+  var fieldsSets = document.querySelectorAll('fieldset');
+  var selectors = document.querySelectorAll('select');
+  var mapFilters = document.querySelector('.map__filters');
+  var formFieldset = adForm.querySelectorAll('fieldset');
+  var formSelect = adForm.querySelectorAll('select');
+  var filterSelect = mapFilters.querySelectorAll('select');
+  var filterFieldset = mapFilters.querySelectorAll('fieldset');
 
   // управляет карточкой объявления
   popupClose.addEventListener('click', function () {
@@ -42,20 +50,96 @@
       mapPinClick(mapPins[i]);
     }
   };
+  // активация формы и поиск координат
+  var toggleStates = function (formElement, isDisabled) {
+    for (var i = 0; i < formElement.length; i++) {
+      formElement[i].disabled = isDisabled;
+    }
+  };
+
+  toggleStates(fieldsSets, true);
+  toggleStates(selectors, true);
+
+  var changeDomElementState = function () {
+    document.querySelector('.map').classList.remove('map--faded');
+    adForm.classList.remove('ad-form--disabled');
+    toggleStates(formFieldset, false);
+    toggleStates(formSelect, false);
+    toggleStates(filterSelect, false);
+    toggleStates(filterFieldset, false);
+  };
+
+  var fieldAddress = document.querySelector('#address');
+  var mapPinY = parseInt(mapPinMain.style.top, 10);
+  var mapPinX = parseInt(mapPinMain.style.left, 10);
+  var mapPinMainWidth = mapPinMain.offsetWidth;
+  var mapPinMainHeight = mapPinMain.offsetHeight;
+
+  var getCoordinatesPinActive = function () {
+    var xCenterActive = mapPinX + mapPinMainWidth / 2;
+    var yCenterActive = mapPinY + mapPinMainHeight;
+    fieldAddress.value = xCenterActive + ', ' + yCenterActive;
+  };
+
+  var getCoordinatesPinDisabled = function () {
+    var xCenterDisabled = mapPinX + mapPinMainWidth / 2;
+    var yCenterDisabled = mapPinY + mapPinMainHeight / 2;
+    fieldAddress.value = xCenterDisabled + ', ' + yCenterDisabled;
+  };
+
+  getCoordinatesPinDisabled();
+
 
   var onActivatePins = function (evt) {
     if (evt.button === 0) {
       window.pin.activate();
       getActivePin();
+
+      mapPinMain.addEventListener('mousedown', function (mouseDownEvt) {
+        mouseDownEvt.preventDefault();
+
+        var startCoords = {
+          x: mouseDownEvt.clientX,
+          y: mouseDownEvt.clientY
+        };
+
+        var onMouseMove = function (moveEvt) {
+          moveEvt.preventDefault();
+
+          var shift = {
+            x: startCoords.x - moveEvt.clientX,
+            y: startCoords.y - moveEvt.clientY
+          };
+
+          startCoords = {
+            x: moveEvt.clientX,
+            y: moveEvt.clientY
+          };
+
+          mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+          mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+        };
+
+        var onMouseUp = function (upEvt) {
+          upEvt.preventDefault();
+
+          document.removeEventListener('mousemove', onMouseMove);
+          document.removeEventListener('mouseup', onMouseUp);
+        };
+
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+      });
     }
 
     mapPinMain.removeEventListener('mousedown', onActivatePins);
   };
 
+
   var onPinMainMousedown = function (evt) {
     if (evt.button === 0) {
-      window.form.changeDomElementState();
-      window.form.getCoordinatesPinActive();
+      changeDomElementState();
+      getCoordinatesPinActive();
       mapPinMain.removeEventListener('mousedown', onPinMainMousedown);
     }
   };
